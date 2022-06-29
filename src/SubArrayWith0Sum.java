9035fa384fe619a56adeaf5f56f498e8e85ed11f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-28 18:54:05 -0700

    completed a method where I find the if the array contains any subarray whose sum in zero.

--- src/SubArrayWith0Sum.java
+++ src/SubArrayWith0Sum.java
@@ -1,5 +1,26 @@
+import java.util.HashMap;
+
 public class SubArrayWith0Sum {
-    
+
+
+    public static boolean subArrayWith0Sum(int arr[], int n){
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+
+            if(sum==0){
+                return true;
+            }
+            if(map.containsKey(sum)){
+                return true;
+            }
+            map.put(sum, true);
+
+        }return false;
+    }
+
 
 
     public static void main(String[] args) {
